558c74cdd449d146e023c95b1ac2935f2b6de393
ErikHeinz <erikheinz933101@outlook.com>
2022-07-04 15:09:53 +0300

    e

--- FE/src/RecordingScreen.tsx
+++ FE/src/RecordingScreen.tsx
@@ -10,8 +10,8 @@ const RecordingScreen: Component = () => {
     const [getElapsedTime, setElapsedTime] = createSignal(0);
     const [getRecordingStep, setRecordingStep] = createSignal(0);
 
-    const baseUrl = "https://vmi873667.contaboserver.net:4000";
-    const websocketUrl = "ws://vmi873667.contaboserver.net/websocket";
+    const baseUrl = "https://vmi873667.contaboserver.net/";
+    const websocketUrl = "wss://vmi873667.contaboserver.net/websocket";
     const captureInterval = 1000;
 
     let mediaRecorder: MediaRecorder;
